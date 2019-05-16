4fda1923408e44f795c3a486d7b6a30ab7a79f43
Anton Danylenko <adanylenko@stuy.edu>
2019-05-15 23:13:30 -0400

    s

--- tictactoe/count.py
+++ tictactoe/count.py
@@ -11,9 +11,9 @@ def printBoard(board):
     for i in range(3):
         row = ""
         for ii in range(3):
-            if board[i*3+ii]==1:
+            if board[i*3+ii]=='1':
                 row+='x '
-            elif board[i*3+ii]==-1:
+            elif board[i*3+ii]=='0':
                 row+='o '
             else:
                 row+='_ '
@@ -63,38 +63,50 @@ class TicTacToe:
 def count_games(board, mark):
     # printBoard(board)
     count = 0
-    i=0
-    while i<9:
-        # printBoard(board)
-        if board[i]=='_':
-            board[i] = mark
-            for clique in cliques:
-                if i in clique:
-                    num_filled=0
-                    # print("clique: ", clique)
-                    for spot in clique:
-                        # print(i, spot, board[spot], mark)
-                        if board[spot]==mark:
-                            num_filled+=1
-                    # print("num_filled: ", num_filled)
-                    if num_filled>2:
-                        # if mark>0:
-                        #     self.win_x+=1
-                        # else:
-                        #     self.win_o+=1
-                        # print("Mark: ", mark)
-                        printBoard(board)
-                        return count+1
-            count+=count_games(board[:],0-mark)
-            # if not i==8:
-            # print("i: ", i)
-            board[i]='_'
+    while '_' in board:
+        printBoard(board)
+        i=0
+        while i<9:
+            # printBoard(board)
+            if board[i]=='_':
+                # print("Mark: ", mark)
+                # print("Before")
+                # printBoard(board)
+                board = board[:i] + str(mark) + board[i+1:]
+                # print("After")
+                # printBoard(board)
+                for clique in cliques:
+                    # print("Clique: ", clique)
+                    if i in clique:
+                        num_filled=0
+                        # print("clique: ", clique)
+                        for spot in clique:
+                            # print(i, spot, board[spot], mark)
+                            if board[spot]==str(mark):
+                                num_filled+=1
+                        # print("num_filled: ", num_filled)
+                        if num_filled>2:
+                            # if mark>0:
+                            #     self.win_x+=1
+                            # else:
+                            #     self.win_o+=1
+                            # print("Mark: ", mark)
+                            # print("Win")
+                            # printBoard(board)
+                            return count+1
+                count+=count_games(board,1-mark)
+                # if not i==8:
+                # print("i: ", i)
+                board = board[:i] + '_' + board[i+1:]
+                # printBoard(board)
             i+=1
+        board = board[:i-1] + str(mark) + board[i:]
     # for i in range(9):
     #     if board[i]
     # self.draw+=1
     # print("Mark: ", mark)
-    printBoard(board)
+    # print("Draw")
+    # printBoard(board)
     return count+1
 
 def main():
@@ -104,7 +116,7 @@ def main():
     # print("Total number of games: ", game.count_games_total(board, 1))
     # print(game)
     #printBoard(board)
-    board = ['_','_','_','_','_','_','_','_','_']
+    board = "_________"
     print("Total number of games 2: ", count_games(board,1))
 
 main()
