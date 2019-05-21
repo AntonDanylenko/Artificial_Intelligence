6dc45303756d269d57d08192ac345f5746f00bd3
AntonDanylenko <adanylenko@stuy.edu>
2019-05-21 14:54:29 -0400

    s

--- tictactoe/count.py
+++ tictactoe/count.py
@@ -13,12 +13,12 @@ def printBoard(board):
         for ii in range(3):
             if board[i*3+ii]==1:
                 row+='x '
-            elif board[i*3+ii]==-1:
+            elif board[i*3+ii]==0:
                 row+='o '
             else:
                 row+='_ '
         print(row)
-    print("--------------------")
+    # print("--------------------")
 
 class TicTacToe:
     def __init__(self):
@@ -32,82 +32,77 @@ class TicTacToe:
 
     def count_games_total(self, board, mark):
         # printBoard(board)
-        if self.test_index==0:
-            print("start count_games_total")
-            print("I: ", self.test_index)
-            printBoard(board)
-            print("----------------------------")
-            self.test_index+=1
+        # if self.test_index==0:
+        #     print("start count_games_total")
+        #     printBoard(board)
+        #     print("----------------------------")
+        #     self.test_index+=1
         count = 0
         for i in range(9):
-            if self.test_index==1:
-                print("for i in range(9)")
-                print("I: ", self.test_index)
-                printBoard(board)
-                print("----------------------------")
+            # if self.test_index==1:
+            #     print("for i in range(9)")
+            #     printBoard(board)
+            #     print("----------------------------")
             # printBoard(board)
             if board[i]=='_':
-                if self.test_index==1:
-                    print("board[i]=='_'")
-                    print("I: ", self.test_index)
-                    printBoard(board)
-                    print("----------------------------")
-                    self.test_index+=1
+                # if self.test_index==1:
+                #     print("board[i]=='_'")
+                #     printBoard(board)
+                #     print("----------------------------")
+                #     self.test_index+=1
                 board[i] = mark
-                if self.test_index==2:
-                    print("board[i]=mark")
-                    print("I: ", self.test_index)
-                    printBoard(board)
-                    print("----------------------------")
-                    self.test_index+=1
+                # if self.test_index==2:
+                #     print("board[i]=mark")
+                #     printBoard(board)
+                #     print("----------------------------")
+                #     self.test_index+=1
+                not_win=True
                 for clique in cliques:
-                    if i in clique:
+                    if i in clique and not_win:
                         num_filled=0
                         # print("clique: ", clique)
                         for spot in clique:
-                            if self.test_index==3:
-                                print("for spot in clique")
-                                print("I: ", self.test_index)
-                                printBoard(board)
-                                print("i: ", i)
-                                print("spot: ", spot)
-                                print("board[spot]: ", board[spot])
-                                print("mark: ", mark)
-                                print("----------------------------")
-                                self.test_index+=1
+                            # if self.test_index==3:
+                            #     print("for spot in clique")
+                            #     printBoard(board)
+                            #     print("i: ", i)
+                            #     print("spot: ", spot)
+                            #     print("board[spot]: ", board[spot])
+                            #     print("mark: ", mark)
+                            #     print("----------------------------")
+                            #     self.test_index+=1
                             # print(i, spot, board[spot], mark)
                             if board[spot]==mark:
                                 num_filled+=1
                         # print("num_filled: ", num_filled)
                         if num_filled>2:
-                            if mark>0:
+                            if mark==1:
                                 self.win_x+=1
+                                # print("Win X")
+                                # printBoard(board)
+                                # print("----------------------------")
                             else:
                                 self.win_o+=1
-                            # if self.test_index==4:
-                            #     print("num_filled>2")
-                            #     print("I: ", self.test_index)
-                            #     printBoard(board)
-                            #     print("Mark: ", mark)
-                            #     print("----------------------------")
-                            #     self.test_index+=1
+                                # print("Win O")
+                                # printBoard(board)
+                                # print("----------------------------")
                             # print("Mark: ", mark)
                             # printBoard(board)
-                            return count+1
-                count+=self.count_games_total(board[:],0-mark)
+                            count+=1
+                            not_win=False
+                count+=self.count_games_total(board[:],1-mark)
                 board[i]='_'
-        self.draw+=1
-        # print("Mark: ", mark)
-        # print("Draw")
-        # printBoard(board)
-        if self.test_index==4:
-            print("Draw")
-            print("I: ", self.test_index)
-            printBoard(board)
-            print("Mark: ", mark)
-            print("----------------------------")
-            self.test_index=0
-        return count+1
+        if not '_' in board:
+            self.draw+=1
+            # if self.test_index==4:
+            # print("Draw")
+            # printBoard(board)
+            # print("Mark: ", mark)
+            # print("----------------------------")
+                # self.test_index=0
+            return 1
+        else:
+            return count
 
 def count_games(board, mark):
     # printBoard(board)
